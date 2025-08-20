e242e1de1d4bc3d7482cf08da043d2db1eac002e
PeymonA <peymon.armand4@gmail.com>
2025-08-20 20:16:57 +1200

    init

---
+++ scraper-javascript.js
@@ -0,0 +1,21 @@
+/**
+ * scraper-javascript.js
+ * To run this script, copy and paste `node scraper-javascript.js` in the terminal
+ */
+
+const cheerio = require('cheerio');
+
+(async () => {
+      const url = 'https://www.example.com';
+    const response = await fetch(url);
+
+    const $ = cheerio.load(await response.text());
+    const title = $('h1').text();
+    const text = $('p').text();
+    const link = $('a').attr('href');
+
+    console.log(title);
+    console.log(text);
+    console.log(link);
+
+})();
